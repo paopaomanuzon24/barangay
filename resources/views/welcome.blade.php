d9e374a95e0aef800a991fec87b8977d948cf279
Paul Manuzon <paulangelo.manuzon@gmail.com>
2021-11-30 21:45:41 +0800

    change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -30,7 +30,8 @@
                         <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
 
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
+                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Tite
+                            </a>
                         @endif
                     @endauth
                 </div>
