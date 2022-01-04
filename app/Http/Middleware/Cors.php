c31ea1dc8fc9b99972cd26812b549568c2e0ae4c
edsheeph <apilanedrian@gmail.com>
2022-01-04 17:38:26 +0800

    Code Fix

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,28 +16,28 @@ class Cors
      */
     public function handle(Request $request, Closure $next)
     {
-        dd("");
-        header("Access-Control-Allow-Origin: *");
+        // dd("");
+        // header("Access-Control-Allow-Origin: *");
 
-        $headers = [
-            'Access-Control-Allow-Methods' => 'GET, POST, PUT, PATCH, DELETE, OPTIONS',
-            'Access-Control-Allow-Headers' => 'Accept, Content-Type, Authorization'
-        ];
+        // $headers = [
+        //     'Access-Control-Allow-Methods' => 'GET, POST, PUT, PATCH, DELETE, OPTIONS',
+        //     'Access-Control-Allow-Headers' => 'Accept, Content-Type, Authorization'
+        // ];
 
-        if ($request->getMethod() == "OPTIONS") {
-            return response()->json('OK', 200, $headers);
-        }
+        // if ($request->getMethod() == "OPTIONS") {
+        //     return response()->json('OK', 200, $headers);
+        // }
 
-        $response = $next($request);
-        foreach ($headers as $key => $value) {
-            $response->header($key, $value);
-        }
+        // $response = $next($request);
+        // foreach ($headers as $key => $value) {
+        //     $response->header($key, $value);
+        // }
 
-        return $response;
+        // return $response;
 
-        // return $next($request)
-        //     ->header('Access-Control-Allow-Origin', '*')
-        //     ->header('Access-Control-Allow-Methods', '')
-        //     ->header('Access-Control-Allow-Headers', '');
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers', 'Accept, Content-Type, Authorization');
     }
 }
