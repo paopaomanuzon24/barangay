531348edfd01b3d049f5cf1fd33d7261e323c91e
edsheeph <apilanedrian@gmail.com>
2022-01-04 15:49:58 +0800

    Code Fix

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -69,6 +69,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'usersession' => \App\Http\Middleware\CheckUserSession::class,
+        'cors' => \App\Http\Middleware\ModifyHeadersMiddleware::class, 
 
     ];
 }
