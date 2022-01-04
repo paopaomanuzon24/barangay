ef5d61fdcb0cb007ab15989c678c29fd09e656f2
edsheeph <apilanedrian@gmail.com>
2022-01-04 15:34:29 +0800

    Code Fix

--- app/Http/Middleware/ModifyHeadersMiddleware.php
+++ app/Http/Middleware/ModifyHeadersMiddleware.php
@@ -18,7 +18,8 @@ class ModifyHeadersMiddleware
     {
         $response = $next( $request );
         $response->header( 'Access-Control-Allow-Origin', '*' );
-        $response->header( 'Access-Control-Allow-Headers', 'Origin, Content-Type' );
+        $response->header( 'Access-Control-Allow-Methods', 'PUT, POST, DELETE, GET, OPTIONS' );
+        $response->header( 'Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type' );
 
         return $response;
     }
