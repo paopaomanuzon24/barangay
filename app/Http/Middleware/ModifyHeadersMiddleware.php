3752f4983c35de1d914bb32da6c5bbd88222961c
edsheeph <apilanedrian@gmail.com>
2022-01-04 16:34:58 +0800

    DD

--- app/Http/Middleware/ModifyHeadersMiddleware.php
+++ app/Http/Middleware/ModifyHeadersMiddleware.php
@@ -16,6 +16,7 @@ class ModifyHeadersMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
+        dd("");
         return $next($request)->header('Access-Control-Allow-Origin', '*')
         ->header('Access-Control-Allow-Methods','GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS')
         ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
