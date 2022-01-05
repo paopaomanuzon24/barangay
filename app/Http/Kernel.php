bb04b399c721b4c61e4a4c2027a65d83a9d6a773
Sebastian Curtis T. Lavarias <sebastiancurtislavarias@gmail.com>
2022-01-05 10:34:07 +0800

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -16,11 +16,11 @@ class Kernel extends HttpKernel
     protected $middleware = [
         // \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
-        // \Fruitcake\Cors\HandleCors::class,
+        \Fruitcake\Cors\HandleCors::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
-        \App\Http\Middleware\Cors::class,
+        // \App\Http\Middleware\Cors::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \Illuminate\Session\Middleware\StartSession::class,
         \App\Http\Middleware\Cors::class,
