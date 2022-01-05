6c31c67a3e0d16a609f59db6609ddccb69a01283
Paul Manuzon <paulangelo.manuzon@gmail.com>
2022-01-05 19:38:16 +0800

    push

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,9 +16,8 @@ class Cors
      */
     public function handle(Request $request, Closure $next)
     {
-
-        return $next($request)->header('Access-Control-Allow-Origin', '*')
-        ->header('Access-Control-Allow-Methods','GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS')
-        ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers', 'Accept, Content-Type, Authorization');
     }
-}
