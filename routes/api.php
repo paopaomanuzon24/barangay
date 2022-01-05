e3ae76416336908789598469ab00162f4719d0ee
edsheeph <apilanedrian@gmail.com>
2022-01-05 09:55:56 +0800

    Code Fix

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+header('Access-Control-Allow-Origin:  *');
+header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
