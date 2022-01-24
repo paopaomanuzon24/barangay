e1deca7d08cb64df612dfc36a5aadf9a0fbe6d24
Paul Manuzon <paulangelo.manuzon@gmail.com>
2022-01-24 16:10:38 +0800

    push

--- app/Http/Controllers/ClearancePaymentMethodController.php
+++ app/Http/Controllers/ClearancePaymentMethodController.php
@@ -9,7 +9,7 @@ class ClearancePaymentMethodController extends Controller
 {
 
     public function list(Request $request){
-        $paymentMethodData = ClearancePaymentMethod::all();
+        $paymentMethodData = ClearancePaymentMethod::where("id",1)->get();
 
         return customResponse()
             ->data($paymentMethodData)
