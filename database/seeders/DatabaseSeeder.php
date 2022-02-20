3a5b0f81461cb96b31174e8b8da11ad757f2ccb4
edsheeph <apilanedrian@gmail.com>
2022-02-20 23:04:11 +0800

    Code Fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             RefProvinceSeeder::class,
             RefCitySeeder::class,
             RefLanguage::class,
+            RefDisease::class,
         ]);
     }
 }
