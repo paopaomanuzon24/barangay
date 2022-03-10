ba41fbcb88eede07ce943284bd2b3b20b5ec6dbf
edsheeph <apilanedrian@gmail.com>
2022-03-10 17:25:23 +0800

    Code Fix

--- app/Classes/AnnouncementClass.php
+++ app/Classes/AnnouncementClass.php
@@ -115,6 +115,7 @@ class AnnouncementClass
         )
         ->join("users", "users.id", "announcements.created_by")
         ->where("announcements.is_city_hall", 1)
+        ->where("announcements.pinned", 1)
         ->with(['images'])
         ->orderBy("announcements.pinned", "desc")
         ->orderBy("announcements.id", "desc");
